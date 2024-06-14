1ecb5bc004595fee86f7db787c39bc9b6e32cd81
speedforce24 <georgeemoefe@gmail.com>
2024-06-14 11:21:12 +0100

    update

--- myapp2/app/layout.tsx
+++ myapp2/app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'TEESAS.com',
   description: 'Welcome to Teesas Education',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+      <a target="_blank" href="https://icons8.com/icon/tKD3C7EOcsTX/knight">Knight</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
